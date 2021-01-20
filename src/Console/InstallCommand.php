4b6a399b1a1d175894948373fae52c4626ed60ca
ivan <ivan@haxibiao.com>
2021-01-20 17:17:51 +0800

    重构所有app nova models到breeze

--- src/Console/InstallCommand.php
+++ src/Console/InstallCommand.php
@@ -13,7 +13,7 @@ class InstallCommand extends Command
      *
      * @var string
      */
-    protected $signature = 'dimension:install';
+    protected $signature = 'dimension:install {--force}';
 
     /**
      * The console command description.
@@ -40,23 +40,6 @@ class InstallCommand extends Command
     public function handle()
     {
         $this->info("复制 stubs ...");
-        $this->copyStubs();
-    }
-
-    public function copyStubs()
-    {
-        //复制所有app stubs
-        foreach (glob(__DIR__ . '/stubs/*.stub') as $filepath) {
-            $filename = basename($filepath);
-            copy($filepath, app_path(str_replace(".stub", ".php", $filename)));
-        }
-        //复制所有nova stubs
-        if (!is_dir(app_path('Nova'))) {
-            mkdir(app_path('Nova'));
-        }
-        foreach (glob(__DIR__ . '/stubs/Nova/*.stub') as $filepath) {
-            $filename = basename($filepath);
-            copy($filepath, app_path('Nova/' . str_replace(".stub", ".php", $filename)));
-        }
+        copyStubs(__DIR__, false);
     }
 }
